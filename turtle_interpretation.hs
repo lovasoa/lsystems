d2dd8f8badc0706d1ca9796e9ef6391a03f4c2f9
Ophir LOJKINE <pere.jobs@gmail.com>
2016-04-29 05:10:18 +0300

    Export svg size

--- turtle_interpretation.hs
+++ turtle_interpretation.hs
@@ -1,27 +1,22 @@
 import Data.List
 
 data Ctx = Ctx {
-                angle :: Float,
-                alpha :: Float,
-                pos :: (Float, Float),
-                size :: Float,
-                saved :: [Ctx],
-                svg :: ShowS
+                angle :: Float, -- current angle
+                alpha :: Float, -- angle incrementation
+                pos :: (Float, Float), -- current position on the canvas
+                size :: (Float, Float), -- canvas size
+                linelen :: Float, -- length of lines
+                saved :: [Ctx], -- saved positions
+                svg :: ShowS -- svg path string
                }
 
-
-svg_start = unlines [
-              "<svg ",
-                "xmlns=\"http://www.w3.org/2000/svg\" ",
-                "xmlns:xlink=\"http://www.w3.org/1999/xlink\"> ",
-              "<path fill=\"none\" stroke=\"black\" d=\"M10,10"]
-svg_end = "\n\" />\n</svg>"
 
 defaultCtx = Ctx {
   angle = 0,
   alpha = pi/2,
   pos = (0,0),
-  size = 100,
+  size = (0,0),
+  linelen = 100,
   saved = [],
   svg = showString ""
 }
@@ -31,11 +26,13 @@ addSvg ctx command (x,y) = ctx { svg = (svg ctx) . showString (command ++ (show 
 
 interpret :: Ctx -> Char -> Ctx
 interpret ctx 'F' = let
-  l = size ctx
+  l = linelen ctx
   a = angle ctx
-  (x,y) = (l*cos(a), l*sin(a))
-  (x', y') = pos ctx
-  in addSvg ctx {pos = (x'+x, y'+y)} "l" (x,y)
+  (dx,dy) = (l*cos(a), l*sin(a))
+  (x, y) = pos ctx
+  (newx, newy) = (x+dx, y+dy) 
+  (h, w) = size ctx
+  in addSvg ctx {pos = (newx,newy), size=(max w newx, max h newy)} "l" (dx,dy)
 
 interpret ctx '-' = ctx {angle = alpha ctx + angle ctx}
 interpret ctx '+' = ctx {angle = angle ctx - alpha ctx}
@@ -53,11 +50,19 @@ interpret ctx ']' = case saved ctx of
 
 interpret ctx _ = ctx
 
-path :: String -> String
-path = ($"") . svg . foldl' interpret defaultCtx
-
 turtle :: String -> String
-turtle s = svg_start ++ (path s) ++ svg_end
+turtle code = let
+              ctx = foldl' interpret defaultCtx code
+              (w',h') = size ctx
+              (w,h) = (w'+20, h'+20)
+              path = svg ctx ""
+           in concat [
+              "<svg ",
+                "xmlns=\"http://www.w3.org/2000/svg\" ",
+                "xmlns:xlink=\"http://www.w3.org/1999/xlink\" ",
+                "width=\"", show w, "\" height=\"", show h, "\" >\n",
+              "<path fill=\"none\" stroke=\"black\" d=\"M10,10",
+                path, "\" />\n</svg>"]
 
 main = do
   source <- getContents
