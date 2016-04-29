ec3b1b38c9d95fccbf64b9c9a2339f7977d36747
Ophir LOJKINE <pere.jobs@gmail.com>
2016-04-29 05:52:38 +0300

    svg viewbox

--- turtle_interpretation.hs
+++ turtle_interpretation.hs
@@ -7,7 +7,7 @@ data Ctx = Ctx {
                 angle :: Float, -- current angle
                 alpha :: Float, -- angle incrementation
                 pos :: (Float, Float), -- current position on the canvas
-                size :: (Float, Float), -- canvas size
+                viewbox :: (Float, Float, Float, Float), -- canvas size
                 linelen :: Float, -- length of lines
                 saved :: [Ctx], -- saved positions
                 svg :: ShowS -- svg path string
@@ -18,7 +18,7 @@ defaultCtx = Ctx {
   angle = 0,
   alpha = pi/2,
   pos = (0,0),
-  size = (0,0),
+  viewbox = (0,0,0,0),
   linelen = 50,
   saved = [],
   svg = showString ""
@@ -34,8 +34,11 @@ interpret ctx 'F' = let
   (dx,dy) = (l*cos(a), l*sin(a))
   (x, y) = pos ctx
   (newx, newy) = (x+dx, y+dy) 
-  (w, h) = size ctx
-  in addSvg ctx {pos = (newx,newy), size=(max w newx, max h newy)} "l" (dx,dy)
+  (minx, miny, w, h) = viewbox ctx
+  in addSvg ctx {
+                  pos = (newx,newy),
+                  viewbox=(min minx newx, min miny newy, max w newx, max h newy)
+                 } "l" (dx,dy)
 
 interpret ctx '-' = ctx {angle = alpha ctx + angle ctx}
 interpret ctx '+' = ctx {angle = angle ctx - alpha ctx}
@@ -56,18 +59,20 @@ interpret ctx _ = ctx
 turtle :: Ctx -> String -> String
 turtle defaultCtx code = let
               ctx = foldl' interpret defaultCtx code
-              (w',h') = size ctx
-              (w,h) = (w'+20, h'+20)
+              (minx, miny, maxx, maxy) = viewbox ctx
+              (w,h) = (maxx-minx+20, maxy-miny+20)
               path = svg ctx ""
            in concat [
-              "<svg ",
+              "<svg\n",
                 "xmlns=\"http://www.w3.org/2000/svg\" ",
-                "xmlns:xlink=\"http://www.w3.org/1999/xlink\" ",
+                "xmlns:xlink=\"http://www.w3.org/1999/xlink\"\n",
+                "viewBox=\"",
+                    show (minx-10), " ", show (miny-10), " ",
+                    show w, " ", show h, "\"\n",
                 "width=\"", show w, "\" height=\"", show h, "\" >\n",
-              "<g transform=\"translate(10,10)\">\n",
               "<path fill=\"none\" stroke=\"black\" d=\"\nM0,0\n",
                 path, "\" />\n",
-              "</g>\n</svg>"]
+              "</svg>"]
 
 main = do
   source <- getContents
